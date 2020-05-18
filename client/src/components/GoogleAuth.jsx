3963f459d7a6ad9fc495d8a93b7acb38aea20fb5
kazitipu <kazi.tipu.nxt@gmail.com>
2020-05-18 11:08:50 +0600

    Added redux form in CreateStream

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -16,7 +16,6 @@ class GoogleAuth extends React.Component {
           this.auth = window.gapi.auth2.getAuthInstance();
           this.onAuthChange(this.auth.isSignedIn.get());
           this.auth.isSignedIn.listen(this.onAuthChange);
-          console.log(this.props.isSignedIn);
         });
     });
   }
--- client/src/components/streams/CreateStream.jsx
+++ client/src/components/streams/CreateStream.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-const CreateStream = () => {
-  return <div> CreateStream</div>;
+class CreateStream extends React.Component {
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+  renderError = ({ touched, error }) => {
+    if (touched && error) {
+      return error;
+    }
+    return null;
+  };
+  renderInput = ({ input, label, meta }) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input
+          placeholder={label}
+          {...input}
+          autoComplete="off"
+          className="form-control"
+        />
+        <div className="form-text text-muted">{this.renderError(meta)}</div>
+      </div>
+    );
+  };
+  render() {
+    return (
+      <div>
+        <h1>CreateStream</h1>
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <div className="form-group">
+            <Field
+              name="title"
+              component={this.renderInput}
+              label="Enter title"
+            />
+          </div>
+          <div className="form-group">
+            <Field
+              name="description"
+              component={this.renderInput}
+              label="Enter description"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
 };
-
-export default CreateStream;
+export default reduxForm({ form: "Streams", validate })(CreateStream);
